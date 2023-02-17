1429edd69550e62b423a168998f3a54122230028
IAmARodent <geronimo.aldana0@gmail.com>
2023-02-16 16:19:12 -0800

    Empty Line Support

--- A1/countnames.c
+++ A1/countnames.c
@@ -7,9 +7,20 @@ int main(void)
     char uniqueNames[100][30];
 
     char line[30];
+    char emptyString[] = "\n";
+    int nameCounter = 0;
     while(NULL != fgets(line, 30, names))
     {
-        printf("%s", line);
+        if(strcmp(line, emptyString) == 0)
+        {
+            printf("Line %d empty.\n", nameCounter+1);
+        }
+        strcpy(uniqueNames[nameCounter], line);
+        nameCounter++;
+    }
+    for(int i = 0; i < nameCounter; i++)
+    {
+        printf("%s", uniqueNames[i]);
     }
     return 0;
 }
